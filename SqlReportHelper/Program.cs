c1bb64ce01d6fb0272756e06a2bbcbf84df93a30
Lukas <yl1882@nyu.edu>
2017-04-13 21:56:16 -0400

    Add project files.

---
+++ SqlReportHelper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SqlReportHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
